f28a5a95f59d3a51d0c1a80eae488bad5cbe4b15
Olaf Bergmann <bergmann@tzi.org>
2016-10-28 10:34:51 +0200

    scripts/fix-unit.pc: added missing stdout redirect

--- scripts/fix-cunit.sh
+++ scripts/fix-cunit.sh
@@ -10,7 +10,7 @@ CUNIT_PC=`dpkg -L libcunit1-dev 2>/dev/null|grep cunit.pc`
 echo "CUNIT_PC: $CUNIT_PC" >&2
 if test "x$CUNIT_PC" != "x"; then
     cat $CUNIT_PC >&2
-    grep -q Version: $CUNIT_PC || (dpkg -l|sed -ne 's/^ii\s\+libcunit1-dev\s\+\([0-9]\+.[0-9]\+-[0-9]\+\).*$/Version: \1/p; T; q'|cat - $CUNIT_PC |sed -e 's/={exec_prefix/=${exec_prefix/' `pwd`/cunit.pc)
+    grep -q Version: $CUNIT_PC || (dpkg -l|sed -ne 's/^ii\s\+libcunit1-dev\s\+\([0-9]\+.[0-9]\+-[0-9]\+\).*$/Version: \1/p; T; q'|cat - $CUNIT_PC |sed -e 's/={exec_prefix/=${exec_prefix/' >`pwd`/cunit.pc)
     echo "`pwd`/cunit.pc is:" >&2
     cat `pwd`/cunit.pc >&2
 fi
