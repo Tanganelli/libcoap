dced7c4d8a685ff399f0987912e8e0e434469751
Olaf Bergmann <bergmann@tzi.org>
2016-10-28 10:28:09 +0200

    scripts/fix-cunit.sh: more debug output

--- scripts/fix-cunit.sh
+++ scripts/fix-cunit.sh
@@ -7,8 +7,12 @@ pkg-config cunit 2>/dev/null && exit 0
 # If not, exit with success (the configure script will raise
 # a more descriptive error).
 CUNIT_PC=`dpkg -L libcunit1-dev 2>/dev/null|grep cunit.pc`
+echo "CUNIT_PC: $CUNIT_PC" >&2
 if test "x$CUNIT_PC" != "x"; then
+    cat $CUNIT_PC >&2
     grep -q Version: $CUNIT_PC || (dpkg -l|sed -ne 's/^ii\s\+libcunit1-dev\s\+\([0-9]\+.[0-9]\+-[0-9]\+\).*$/Version: \1/p; T; q'|cat - $CUNIT_PC |sed -e 's/={exec_prefix/=${exec_prefix/' `pwd`/cunit.pc)
+    echo "`pwd`/cunit.pc is:" >&2
+    cat `pwd`/cunit.pc >&2
 fi
 
 # output location of generated cunit.pc
